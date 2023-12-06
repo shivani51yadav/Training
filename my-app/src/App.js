e46e6f5751077e7c10f4a39482f778977844ae41
shivani51yadav <shivani51yadav@gmail.com>
2023-12-06 12:12:13 +0530

    changes in some files of myApp

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
 
 function App() {
-  <>
-  <App/>
-  </>
+  return(
+  <div>
+    <Header/>
+    <Main/>
+    <Footer/>
+  </div>
+  )
 }
 
 export default App;
