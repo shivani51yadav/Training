79594ceef83160ac538b3580f2aeeafc8b6b2424
shivani51yadav <shivani51yadav@gmail.com>
2023-12-09 11:03:24 +0530

    complete

--- my-app/src/UserData.js
+++ my-app/src/UserData.js
@@ -3,6 +3,13 @@ import {Button, Table} from 'react-bootstrap';
 
 const UserData = () => {
     const [data,setData] = useState([])
+    const [svalue,setSvalue]=useState([])
+
+    const searchData=async()=>{
+        const udata = await fetch(`https://reqres.in/api/users/${svalue}`);
+        const udata1 = await udata.json()
+        alert(udata1.data.id + ","+udata1.data.first_name)
+    }
     const getData=async()=>{
         const udata = await fetch('https://reqres.in/api/users?page2');
         const udata1 = await udata.json()
@@ -15,7 +22,8 @@ const UserData = () => {
   return (
     <div>
      User Data
-    <Button variant="primary" onClick={()=>getData()}>GetData</Button><br/>
+    <input type="number" value={svalue} onChange={(e)=>setSvalue(e.target.value)} /><Button variant="primary" onClick={()=>searchData()}>Search data
+    </Button><br/>
     <Table striped bordered hover>
       <thead>
         <tr>
