18110396d4f544833bfb2eda17b551b4d7c0a414
shivani51yadav <shivani51yadav@gmail.com>
2023-12-12 12:05:32 +0530

    some changes in project

--- internship_project/backend/index.js
+++ internship_project/backend/index.js
@@ -1,3 +1,59 @@
+import express from 'express';
+import cors from 'cors';
+import morgan from "morgan";
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+import connectDB from './config/connectDB.js';
+
+
+dotenv.config();
+
+
+//mongoDB connection
+connectDB();
+
+
+//rest object
+const app=express();
+
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+//routes
+app.get('/',(req,res)=>{
+    res.status(200).send({
+        "message":"Node Server"
+    })
+});
+
+
+app.get('/test',(req,res)=>{
+    res.status(200).send({
+        "message":"Node Server Test"
+    })
+});
+
+
+
+//port
+const PORT=process.env.PORT || 8000
+
+//listen
+app.listen(8080,()=>{
+    console.log(`Server Running`, bgCyan.white)
+})
+
+
+
+
+
+
+
 console.log('hello')
 
 let a=10, b=20,c;
